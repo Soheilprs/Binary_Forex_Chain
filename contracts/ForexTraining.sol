95e07e1e578ec35ca14d56500cf7774f82ea9bee
1-Dex <1dex@blockto.ir>
2024-02-08 01:24:14 +0330

    change contract name to Forex_Training

--- contracts/ForexTraining.sol
+++ contracts/ForexTraining.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract Forex_Training_3 is Context, ReentrancyGuard {
+contract Forex_Training is Context, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     struct Node {
